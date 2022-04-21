70530d9336f31f289459afb65d2f4c3059568e8b
Honghua Zhu <zhh200910@gmail.com>
2022-04-21 08:45:16 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -14,11 +14,9 @@ if /i "%ARG%" == "-ec" goto ec
 if /i "%ARG%" == "-es" goto es
 if /i "%ARG%" == "-p" goto p
 if /i "%ARG%" == "-pc" goto pc
-if /i "%ARG%" == "-pd" goto pd
 if /i "%ARG%" == "-i" goto i
 if /i "%ARG%" == "-c" goto c
 if /i "%ARG%" == "-dt" goto dt
-if /i "%ARG%" == "-vu" goto vu
 
 goto usage
 
@@ -28,13 +26,11 @@ echo    options:
 echo    -e            mvn eclipse:eclipse
 echo    -ec           mvn eclipse:clean
 echo    -es           mvn eclipse:eclipse -DdownloadSources=true
-echo    -p            mvn package assembly:assembly -Dmaven.test.skip=true
-echo    -pc           mvn clean package assembly:assembly -Dmaven.test.skip=true
-echo    -pd           mvn package assembly:assembly -Dmaven.test.skip=true -P database
+echo    -p            mvn package -Dmaven.test.skip=true
+echo    -pc           mvn clean package -Dmaven.test.skip=true
 echo    -i            mvn install -Dmaven.test.skip=true
 echo    -c            mvn clean
 echo    -dt           mvn dependency:tree
-echo    -vu version   pom.xml version update
 goto end
 
 :e
@@ -50,15 +46,11 @@ call mvn eclipse:eclipse -DdownloadSources=true
 goto end
 
 :p
-call mvn package assembly:assembly -Dmaven.test.skip=true
+call mvn package -Dmaven.test.skip=true
 goto end
 
 :pc
-call mvn clean package assembly:assembly -Dmaven.test.skip=true
-goto end
-
-:pd
-call mvn package assembly:assembly -Dmaven.test.skip=true -P database
+call mvn clean package -Dmaven.test.skip=true
 goto end
 
 :c
@@ -71,13 +63,6 @@ goto end
 
 :dt
 call mvn dependency:tree
-goto end
-
-:vu
-set VERSION=%2
-if /i "%VERSION%" == "" goto usage
-call mvn versions:set -DnewVersion=%VERSION%
-call mvn versions:commit
 goto end
 
 :end
